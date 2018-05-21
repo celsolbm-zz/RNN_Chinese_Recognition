f896b62e34bcb94eba64675ba0bf0d658c1951bb
Celso <Celso@Megazord.local>
2018-05-21 16:32:43 +0900

    added coments on rnn_regular

--- rnn_regular.py
+++ rnn_regular.py
@@ -16,30 +16,30 @@ tf.set_random_seed(0)
 
 SEQLEN = 270
 BATCHSIZE = 1000
-ALPHASIZE = 6
-BETASIZE = 3756
-INTERNALSIZE = 100
+ALPHASIZE = 6          #size of the tuple
+BETASIZE = 3756        #number of classes
+INTERNALSIZE = 100     #size of the state and gate weights
 NLAYERS = 1
-learning_rate = 0.0001  # fixed learning rate
+learning_rate = 0.001  # fixed learning rate, varied depending on the number of iterations
 dropout_pkeep = 0.8    # some dropout
-L=200
-char_class=3756
+L=200 
+char_class=3756        #not sure what this is for..maybe vestigial. I think you can remove
 
-with open ('chlist', 'rb') as fp:
+with open ('chlist', 'rb') as fp:   #loads from pickle the full training dataset chlist = character list
     chlist = pickle.load(fp)
 
 
-with open ('lbl1h', 'rb') as fp:
+with open ('lbl1h', 'rb') as fp:    #loads the classification values for the full training dataset lbl1h= label 1 hot
     lbl1h = pickle.load(fp)
 
 
-usls=[0,0,0,0,0,0]
-first=True
+usls=[0,0,0,0,0,0]                  #loading zeros for streching due to characters of different 
+first=True			    
 i=0
 for i in range (len(chlist)):
     lstc=chlist[i]
     while len(lstc)<270:
-        lstc.append(usls)
+        lstc.append(usls)           #just filling the list
 
 
 with tf.device('/device:GPU:0'):
@@ -53,37 +53,27 @@ with tf.device('/device:GPU:0'):
     # input state
     Hinf = tf.placeholder(tf.float32, [None, INTERNALSIZE*NLAYERS], name='Hinf')  # [ BATCHSIZE, INTERNALSIZE * NLAYERS]
     Hinb = tf.placeholder(tf.float32, [None, INTERNALSIZE*NLAYERS], name='Hinb')
-    # How to properly apply dropout in RNNs: see README.md
-    cellfwd = rnn.GRUCell(INTERNALSIZE)
-    dcellfwd = rnn.DropoutWrapper(cellfwd,input_keep_prob=1)
-    cellbwd = rnn.GRUCell(INTERNALSIZE)
-    dcellbwd = rnn.DropoutWrapper(cellbwd,input_keep_prob=1)
-    #multicell = rnn.DropoutWrapper(multicell, output_keep_prob=pkeep)
-    # "naive dropout" implementation
-    #dropcells = [rnn.DropoutWrapper(cell,input_keep_prob=pkeep) for cell in cells]
-    #multicell = rnn.MultiRNNCell(dropcells, state_is_tuple=False)
-    #multicell = rnn.DropoutWrapper(multicell, output_keep_prob=pkeep)  # dropout for the softmax layer
-    Yr, H = tf.nn.bidirectional_dynamic_rnn(dcellfwd,dcellbwd, Xf, dtype=tf.float32, initial_state_fw=Hinf,initial_state_bw=Hinb)
-    # Yr: [ BATCHSIZE, SEQLEN, INTERNALSIZE ]
-    # H:  [ BATCHSIZE, INTERNALSIZE*NLAYERS ] # this is the last state in the sequence
+    cellfwd = rnn.GRUCell(INTERNALSIZE)   #Forward cell
+    dcellfwd = rnn.DropoutWrapper(cellfwd,input_keep_prob=0.8)  #dropout for forward cell
+    cellbwd = rnn.GRUCell(INTERNALSIZE)   #Backwards cell
+    dcellbwd = rnn.DropoutWrapper(cellbwd,input_keep_prob=0.8)  #dropout for backward cell
+    Yr, H = tf.nn.bidirectional_dynamic_rnn(dcellfwd,dcellbwd, Xf, dtype=tf.float32, initial_state_fw=Hinf,initial_state_bw=Hinb) #bidirectional rnn calculation
     H = tf.identity(H, name='H')  # just to give it a name
     Yr = tf.identity(Yr, name='Yr')
-    #Hf = tf.identity(H[0], name='Hf')  # just to give it a name
-    #Hb = tf.identity(H[1], name='Hb')
     Yrta2=tf.add(Yr[0],Yr[1],name='Yrta2')
-    Yrta2=tf.div(Yrta2,2)
-    Ypoo=tf.layers.average_pooling1d(Yrta2,[3],strides=[3])
-    Yrr = tf.reshape(Ypoo, [BATCHSIZE,INTERNALSIZE*90],name='Yrr')
-    Y1=tf.contrib.layers.fully_connected(Yrr,L,activation_fn=tf.nn.sigmoid)
-    Y2=tf.contrib.layers.fully_connected(Y1,char_class,activation_fn=None)
+    Yrta2=tf.div(Yrta2,2)   #averaging the 2 cells result (as it is done in the article)
+    Ypoo=tf.layers.average_pooling1d(Yrta2,[3],strides=[3])   #applying pooling, I used 3 here but other values can be tested of course
+    Yrr = tf.reshape(Ypoo, [BATCHSIZE,INTERNALSIZE*90],name='Yrr') #this 90 is due to 270/3. If you change the pooling size change this too
+    Y1=tf.contrib.layers.fully_connected(Yrr,L,activation_fn=tf.nn.sigmoid) #first fully connected layer
+    Y2=tf.contrib.layers.fully_connected(Y1,char_class,activation_fn=None)  #output fully connected layer
     Yf = tf.nn.softmax(Y2)
-    entropy_list = -tf.log(BETASIZE+0.)-tf.reduce_mean(tf.log(Yf),1)
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Y2, labels=Yo_)
-    cross_entropy = tf.reduce_mean(cross_entropy)*100
-    correct_prediction = tf.equal(tf.argmax(Yf, 1), tf.argmax(Yo_, 1))
+    entropy_list = -tf.log(BETASIZE+0.)-tf.reduce_mean(tf.log(Yf),1)        #for the cmcl measurements 
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Y2, labels=Yo_) #normal cross entropy error function
+    cross_entropy = tf.reduce_mean(cross_entropy)*100       
+    correct_prediction = tf.equal(tf.argmax(Yf, 1), tf.argmax(Yo_, 1))             #simple check if prediction is ok
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
-    cp_grad=tf.train.AdamOptimizer(lr).compute_gradients(cross_entropy)
+    train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)                #training
+    cp_grad=tf.train.AdamOptimizer(lr).compute_gradients(cross_entropy)            #vestigial
 
 
 # init
@@ -95,14 +85,14 @@ sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 sess.run(init)
 step = 0
 epoch=0
-
-init_all=False
+ 
+init_all=False   #just used to save the variables before training
 ind=0
 ind2=BATCHSIZE
 start_time=time.time()
 
 
-def unison_shuffled_list(a, b):
+def unison_shuffled_list(a, b):   #used to shuffle the data set
     c = list(zip(a, b))
     random.shuffle(c)
     a, b = zip(*c)
@@ -112,7 +102,7 @@ def unison_shuffled_list(a, b):
 
 
 
-chlist,lbl1h=unison_shuffled_list(chlist,lbl1h)
+chlist,lbl1h=unison_shuffled_list(chlist,lbl1h) #shuffle before training
 for i in range(50000+1):
     if i>10000:
         learning_rate=0.0005
